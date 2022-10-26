32fb15fb0eb83727a2ab701c2c3269c929149841
BobbyKJH <rlatnguscjsw@gmail.com>
2022-10-26 15:17:41 +0900

    Feat: React-query API function

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,25 @@
 import axios from "axios";
 
+const BASE_URL = "https://api.coinpaprika.com/v1";
+
 export const fetchCoins = async () => {
-  const { data } = await axios.get("https://api.coinpaprika.com/v1/coins");
+  const { data } = await axios.get(`${BASE_URL}/coins`);
+  return data;
+};
+
+export const fetchCoinInfo = async (coinId: string | undefined) => {
+  const { data } = await axios.get(`${BASE_URL}/coins/${coinId}`);
+  return data;
+};
+
+export const fetchCoinTickers = async (coinId: string | undefined) => {
+  const { data } = await axios.get(`${BASE_URL}/tickers/${coinId}`);
+  return data;
+};
+
+export const fetchCoinHistory = async (coinId: string | undefined) => {
+  const { data } = await axios.get(
+    `https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`
+  );
   return data;
 };
